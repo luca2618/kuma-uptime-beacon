3d0a5945ae4e421d6a0f0d1f541997ad03615472
Lucas Sylvester <lucassylvester02@gmail.com>
2025-10-24 02:06:25 -0400

    Dummy class for rpio during development

---
+++ dummy/GPIO.py
@@ -0,0 +1,11 @@
+def setmode(mode):
+    pass
+def setup(pin, direction):
+    pass
+def input(pin):
+    return False # Default return value
+def output(pin, value):
+    pass
+def cleanup():
+    pass
+# Add other functions as needed
